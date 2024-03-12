11e50209b66da5ae33aba00f9b10dd2284e293d2
Michael Fink <40691176+mikmikecom@users.noreply.github.com>
2024-03-12 11:32:27 +0100

    Update 1_Project_question1_ver1-final.sql

--- 1_Project_question1_ver1-final.sql
+++ 1_Project_question1_ver1-final.sql
@@ -18,3 +18,28 @@ FROM 	(
 WHERE sub.trend = 'klesá'
 ORDER BY sub.research_category
 ;
+
+
+-- better option
+WITH 
+wage_declining AS (
+	SELECT 
+		tmf.common_years 
+		,tmf.research_category
+		,tmf.avg_value AS avg_wage
+		,LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years) AS avg_value_before_year
+		,round((tmf.avg_value - LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years)) / (LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years)) *100, 2) AS ratio		
+		,CASE 
+			WHEN tmf.avg_value > LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years) THEN 'roste'
+			WHEN tmf.avg_value < LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years) THEN 'klesá'
+			WHEN tmf.avg_value = LAG(tmf.avg_value) OVER (PARTITION BY tmf.research_category ORDER BY tmf.common_years) THEN 'stagnuje'
+			ELSE 'rok 0'
+		 END trend
+	FROM t_michael_fink_project_sql_primary_final tmf
+	WHERE tmf.research_subject = 'mzda'
+)
+SELECT *
+FROM wage_declining
+WHERE trend = 'klesá'
+ORDER BY research_category
+;
